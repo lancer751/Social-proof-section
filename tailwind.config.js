bc3b7d99610c6ddfec486820d376c32cd8bb3a34
lancerlot751 <pachecolau27@gmail.com>
2024-03-29 01:20:59 -0500

    primer avance

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,21 @@
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
+    colors:{
+      "Very-Dark-Magenta": "hsl(300, 43%, 22%)",
+      "Soft-Pink": "hsl(333, 80%, 67%)",
+      "Dark-Grayish-Magenta": "hsl(303, 10%, 53%)",
+      "Light-Grayish-Magenta": "hsl(300, 24%, 96%)",
+      "white": "hsl(0, 0%, 100%)",
+    },
+    fontFamily:{
+      "League-Spartan": ['League Spartan', 'sans-serif'],
+    },
+    backgroundImage:{
+      "pattern-top-mobile": "url('../images/bg-pattern-top-mobile.svg')"
+    },
     extend: {
-      colors:{
-        "Very-Dark-Magenta": "hsl(300, 43%, 22%)",
-        "Soft-Pink": "hsl(333, 80%, 67%)",
-        "Dark-Grayish-Magenta": "hsl(303, 10%, 53%)",
-        "Light-Grayish-Magenta": "hsl(300, 24%, 96%)",
-      },
-      fontFamily:{
-        "League Spartan": ['League Spartan', 'sans-serif'],
-      }
+      
     },
   },
   plugins: [],
